daad1dc18ba9492c6bd0b7aea94f10c8dd3b6645
achiang <jecky_hindrawan123@yahoo.com>
2020-09-16 10:11:48 +0700

    Week4 (@Achiang04)

--- Latihan/Week4/Day1/react-js/src/components/card.js
+++ Latihan/Week4/Day1/react-js/src/components/card.js
@@ -3,16 +3,22 @@ import "./App.css";
 // import { Card, CardTitle, CardText, CardImg } from "reactstrap";
 import { Card } from "react-bootstrap";
 
-export default class App extends Component {
+export default class card extends Component {
   constructor(props) {
     super(props);
   }
   render() {
+    const data = {
+      content: {
+        body: [
+          {
+            cardImg: (src = "./gambar/1.jpeg"),
+          },
+        ],
+      },
+    };
     return (
       <div>
-        <div className="title">
-          <h1>React Native Students Batch#7</h1>
-        </div>
         <div>
           <Card style={{ width: "18rem" }}>
             <Card.Img variant="top" src="./gambar/1.jpeg" />
